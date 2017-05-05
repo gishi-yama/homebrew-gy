3ca22e86ccca77c24c97d9cb919a75555d6910e2
Hiroto Yamakawa <cist.yamakawa@gmail.com>
2017-05-05 13:36:02 +0900

    add myukkurivoice

---
+++ myukkurivoice.rb
@@ -0,0 +1,10 @@
+cask 'myukkurivoice' do
+  version '0.3.21'
+  sha256 '87f2974d7cd158a1fd3308222257db1b342557392f942358efde49b5a15a85f5'
+
+  url 'https://github.com/taku-o/myukkurivoice/releases/download/#{version}/myukkurivoice-darwin-x64.zip'
+  name 'MYukkuriVoice'
+  homepage 'https://github.com/taku-o/myukkurivoice'
+
+  app 'myukkurivoice.app'
+end
