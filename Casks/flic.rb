e739bf03fbccdc60de8cf8bc086a9f7e3f9956ac
Hiroto Yamakawa <cist.yamakawa@gmail.com>
2017-05-08 14:46:51 +0900

    Add flic.1.0.4.app

---
+++ Casks/flic.rb
@@ -0,0 +1,10 @@
+cask 'flic' do
+  version '1.0.4'
+  sha256 'c4245d04cdb7df235f2f98dca321ef26e9b3963c2519bce6fbab24792bbb73c4'
+
+  url "https://misc-scl-cdn.s3.amazonaws.com/Flic.#{version}.dmg"
+  name 'Flic'
+  homepage 'https://flic.io/mac-app/'
+
+  app 'Flic.app'
+end
