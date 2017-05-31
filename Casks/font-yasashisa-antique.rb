8b8766f89228c177d547bc5d03525196bd7cc301
Hiroto Yamakawa <cist.yamakawa@gmail.com>
2017-05-31 22:02:12 +0900

    add font-yasashisa-antique

---
+++ Casks/font-yasashisa-antique.rb
@@ -0,0 +1,10 @@
+cask 'font-yasashisa-antique' do
+  version :latest
+  sha256 'd21a86ba53522dff39b90fe78417dec34eaa29dc8798b2b78009d64fc7c482c2'
+
+  url "http://flop.sakura.ne.jp/font/fontna-op/YasashisaAntiqueFont.zip"
+  name 'YasashisaAntique'
+  homepage 'http://www.fontna.com/'
+
+  font "YasashisaAntiqueFont/07YasashisaAntique.otf"
+end
