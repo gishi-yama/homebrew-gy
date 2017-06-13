554eb722bf14972d6b7f4ad6a15d4ed4c457a96e
Hiroto Yamakawa <cist.yamakawa@gmail.com>
2017-06-13 20:36:46 +0900

    myukkurivoice to v0.3.22

--- Casks/myukkurivoice.rb
+++ Casks/myukkurivoice.rb
@@ -1,5 +1,5 @@
 cask 'myukkurivoice' do
-  version '0.3.21'
+  version '0.3.22'
   sha256 '87f2974d7cd158a1fd3308222257db1b342557392f942358efde49b5a15a85f5'
 
   url "https://github.com/taku-o/myukkurivoice/releases/download/#{version}/myukkurivoice-darwin-x64.zip"
