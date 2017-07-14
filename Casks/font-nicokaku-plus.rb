ebc9354bddfe7ebd4bbf91f0b6ba2509fde5ee22
Hiroto Yamakawa <cist.yamakawa@gmail.com>
2017-07-14 18:26:15 +0900

    add 丸文字「ニコ角」フォント

---
+++ Casks/font-nicokaku-plus.rb
@@ -0,0 +1,10 @@
+cask 'font-nicokaku-plus' do
+  version :latest 
+  sha256 '4bed46b1627db0f8ef25c76a87bf6a2824192a2dd381bab86b569155ed311d41'
+
+  url 'http://nicofont.pupu.jp/nicomoji_u_2525/nicokaku-plus.zip'
+  name 'nicokaku-plus'
+  homepage 'http://nicofont.pupu.jp/nicokaku-plus.html'
+
+  font 'nicokaku-plus/nicokaku_v1.ttf'
+end
