a99347e92f303012f0e126fe4d4c582cd9ccc58b
Hiroto Yamakawa <cist.yamakawa@gmail.com>
2017-07-19 20:40:26 +0900

    add logofonts

---
+++ Casks/font-logofont-b.rb
@@ -0,0 +1,10 @@
+cask 'font-logofont-b' do
+  version :latest 
+  sha256 '6380f0256355a15cc54c7af86f5d69f8833ce1feac2b354a5ebbb58a1bb502f3'
+
+  url 'http://logotype.jp/wp-content/uploads/2015/12/logofont-b_1.1-ah9kl7yha.zip'
+  name 'logofont-b'
+  homepage 'http://logotype.jp/corporate-logo-font-dl.html'
+
+  font 'logofont-b_1.1-ah9kl7yha/logotypejp_mp_b_1.1.ttf'
+end
---
+++ Casks/font-logofont-m.rb
@@ -0,0 +1,10 @@
+cask 'font-logofont-m' do
+  version :latest
+  sha256 'a9176d686bf0623dc062f887239dc1f07f2d0b5c9e71cb5bcbf7770053857a5e'
+
+  url 'http://logotype.jp/wp-content/uploads/2015/12/logofont-m_1.1-ah9kl7yha.zip'
+  name 'logofont-m'
+  homepage 'http://logotype.jp/corporate-logo-font-dl.html'
+
+  font 'logofont-m_1.1-ah9kl7yha/logotypejp_mp_m_1.1.ttf'
+end
