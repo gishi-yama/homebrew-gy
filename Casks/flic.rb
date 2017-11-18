43642096868bcee407e7c102eb2bdf794b553ead
Hiroto Yamakawa <cist.yamakawa@gmail.com>
2017-11-18 23:10:55 +0900

    Upgrade 1.0.7 and Fix style

--- Casks/flic.rb
+++ Casks/flic.rb
@@ -1,16 +1,16 @@
 cask 'flic' do
-  version '1.0.4'
-  sha256 'c4245d04cdb7df235f2f98dca321ef26e9b3963c2519bce6fbab24792bbb73c4'
+  version '1.0.7'
+  sha256 '5aa44b4cb6c3af182e5498d593f12b928e8088f43e318d705c33a8dda0aa0732'
 
-  url "https://misc-scl-cdn.s3.amazonaws.com/Flic.#{version}.dmg"
+  # s3.amazonaws.com/misc-scl-cdn was verified as official when first introduced to the cask
+  url "https://s3.amazonaws.com/misc-scl-cdn/Flic.#{version}.zip"
   name 'Flic'
   homepage 'https://flic.io/mac-app/'
 
-  app 'Flic.app'
+  app 'FlicMac Developer Distribution 2017-05-22 13-09-54/Flic.app'
 
   zap delete: [
-	'~/Library/Application Scripts/com.shortcutlabs.FlicMac',
-	'~/Library/Containers/com.shortcutlabs.FlicMac'
-              ]  
-
+                '~/Library/Application Scripts/com.shortcutlabs.FlicMac',
+                '~/Library/Containers/com.shortcutlabs.FlicMac',
+              ]
 end
