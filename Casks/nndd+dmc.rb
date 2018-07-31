66b6400a4b0182bbe1b11596febb6930b175dd2a
Hiroto Yamakawa <cist.yamakawa@gmail.com>
2018-07-31 14:03:42 +0900

    upgrade nndd+dmc 2.8.0a

--- Casks/nndd+dmc.rb
+++ Casks/nndd+dmc.rb
@@ -1,8 +1,8 @@
 cask 'nndd+dmc' do
-  version '2.7.1'
-  sha256 'e863af20d7442553fc711170557c59f2eff10709c44076754daf4dc00a891c5c'
+  version '2.8.0'
+  sha256 '9b342d1a62c5c5f68a7ce74810a1e0db3d20f23172c4499ed14f19e2fadd99ab'
 
-  url "https://github.com/SSW-SCIENTIFIC/NNDD/releases/download/v#{version}-Beta/NNDD_v#{version.dots_to_underscores}.dmg"
+  url "https://github.com/SSW-SCIENTIFIC/NNDD/releases/download/v#{version}-Alpha/NNDD_v#{version.dots_to_underscores}.dmg"
   appcast 'https://github.com/SSW-SCIENTIFIC/NNDD/releases.atom'
   name 'NNDD+DMC'
   homepage 'https://github.com/SSW-SCIENTIFIC/NNDD'
