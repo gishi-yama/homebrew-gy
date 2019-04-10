7f6c357f89739dcfb01e59a2fc57b0c7b068c3cc
Hiroto Yamakawa <1794095+gishi-yama@users.noreply.github.com>
2019-04-10 20:14:14 +0900

    change download path of docuworks-viewer-light

--- Casks/docuworks-viewer-light.rb
+++ Casks/docuworks-viewer-light.rb
@@ -2,7 +2,7 @@ cask 'docuworks-viewer-light' do
   version '8.0.0'
   sha256 '52d019747a657dcd0e5c182c2b9d634a98adcadd9a84493cfdd77c2ae3722b82'
 
-  url 'http://download.fujixerox.co.jp/pub/exe/docuworks/DWVL800.dmg'
+  url 'https://assets.fujixerox.co.jp/download/pub/exe/docuworks/1/DWVL800.dmg'
   name 'DocuWorks Viewer Light Mac JP'
   homepage 'https://www.fujixerox.co.jp/download/software/docuworks/download102'
 
