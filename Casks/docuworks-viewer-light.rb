02a0ae786516175e4d932c186a90b5d7d2602782
Hiroto Yamakawa <1794095+gishi-yama@users.noreply.github.com>
2019-11-14 17:41:24 +0900

    upgrade docuworks-viewer-light to 9.0.0

--- Casks/docuworks-viewer-light.rb
+++ Casks/docuworks-viewer-light.rb
@@ -1,12 +1,12 @@
 cask 'docuworks-viewer-light' do
-  version '8.0.0'
-  sha256 '52d019747a657dcd0e5c182c2b9d634a98adcadd9a84493cfdd77c2ae3722b82'
+  version '9.0.0'
+  sha256 'a42b8f66743a9338c2d4a87492678f24f22dafc4fc67fa76afca4d6d4b943e5c'
 
-  url 'https://assets.fujixerox.co.jp/download/pub/exe/docuworks/1/DWVL800.dmg'
+  url 'https://assets.fujixerox.co.jp/download/pub/exe/docuworks/1/dwvl900.dmg'
   name 'DocuWorks Viewer Light Mac JP'
   homepage 'https://www.fujixerox.co.jp/download/software/docuworks/download102'
 
   pkg 'DWViewerLight.pkg', allow_untrusted: true
 
-  uninstall pkgutil: 'com.fujixerox.docuworks.dwvl'
+  uninstall pkgutil: 'jp.co.fujixerox.docuworks.dwvlt'
 end
