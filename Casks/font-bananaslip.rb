18720b6c4dda14eac4837aa549644d734cd0df1f
Hiroto Yamakawa <1794095+gishi-yama@users.noreply.github.com>
2020-01-25 21:45:12 +0900

    add font-bananaslip

---
+++ Casks/font-bananaslip.rb
@@ -0,0 +1,10 @@
+cask 'font-bananaslip' do
+  version :latest
+  sha256 '8f06fb35e3bfbcee802ab195269d44a1db5a4794749ce66cca29e815d5f932b0'
+
+  url 'http://ymnk-design.com/wordpress/download/bananaslip/?wpdmdl=66'
+  name 'bananaslip'
+  homepage 'http://ymnk-design.com/wordpress/bananaslip/'
+
+  font 'bananaslip_free/bananaslip.otf'
+end
