87a3ca635b556e792287fd177bc6436d8292212c
Hiroto Yamakawa <1794095+gishi-yama@users.noreply.github.com>
2021-06-10 20:42:23 +0900

    Bump up Docuworks Viewer Light to 9.1.0

--- Casks/docuworks-viewer-light.rb
+++ Casks/docuworks-viewer-light.rb
@@ -1,10 +1,10 @@
 cask 'docuworks-viewer-light' do
-  version '9.0.0'
-  sha256 'a42b8f66743a9338c2d4a87492678f24f22dafc4fc67fa76afca4d6d4b943e5c'
+  version '9.1.0'
+  sha256 'bfbdb6f36bb2b176cdba1bcc7c568077fe8b8d5beff4d6b23f9d1f8ef82edb19'
 
-  url 'https://assets.fujixerox.co.jp/download/pub/exe/docuworks/1/dwvl900.dmg'
+  url 'https://assets-fb.fujifilm.com/download/pub/exe/docuworks/1/dwvl910.dmg'
   name 'DocuWorks Viewer Light Mac JP'
-  homepage 'https://www.fujixerox.co.jp/download/software/docuworks/download102'
+  homepage 'https://www.fujifilm.com/fb/download/software/docuworks/download102'
 
   pkg 'DWViewerLight.pkg', allow_untrusted: true
 
