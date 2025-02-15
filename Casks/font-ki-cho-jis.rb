f18c491fdb82df11f131b11e3300fb64739419f7
Hiroto Yamakawa <1794095+gishi-yama@users.noreply.github.com>
2025-02-16 02:33:20 +0900

    Adjust version No.

--- Casks/font-ki-cho-jis.rb
+++ Casks/font-ki-cho-jis.rb
@@ -1,5 +1,5 @@
 cask 'font-ki-cho-jis' do
-  version :latest
+  version '0.301'
   sha256 '55fe1a39a5d4a5c80903838f2fbc92409a999eeaf80b00ddc35c8d3c3217b4ee'
 
   url "https://font.kim/ki-cho-jis_0301.zip"
