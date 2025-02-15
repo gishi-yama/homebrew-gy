f709ec6ea616c4c8a929deab3956d12470f5367b
Hiroto Yamakawa <1794095+gishi-yama@users.noreply.github.com>
2025-02-16 02:30:11 +0900

    Add 機械彫刻用標準書体フォント
    - https://font.kim/

---
+++ Casks/font-ki-cho-jis.rb
@@ -0,0 +1,10 @@
+cask 'font-ki-cho-jis' do
+  version :latest
+  sha256 '55fe1a39a5d4a5c80903838f2fbc92409a999eeaf80b00ddc35c8d3c3217b4ee'
+
+  url "https://font.kim/ki-cho-jis_0301.zip"
+  name 'ki-cho-jis'
+  homepage 'https://font.kim/'
+
+  font 'KikaiChokokuJIS-Md.otf'
+end
