bd9ab4ff5aa842ebba093955c0e378e61a140941
gishi-yama <yamakawa@photon.chitose.ac.jp>
2026-09-13 11:21:46 +0900

    nndd+dmcとoki_c332dnwを無効化
    使わなくなったため disable! を追加。参照用にファイルは残す。

--- Casks/nndd+dmc.rb
+++ Casks/nndd+dmc.rb
@@ -1,17 +1,20 @@
-cask 'nndd+dmc' do
-  version '3.0.11'
-  sha256 '0e2461344f6afd6ded213cdc0e80c4a4462abcac35ef72424ca5f89e276020a3'
+cask "nndd+dmc" do
+  version "3.0.11"
+  sha256 "0e2461344f6afd6ded213cdc0e80c4a4462abcac35ef72424ca5f89e276020a3"
 
   url "https://github.com/SSW-SCIENTIFIC/NNDD/releases/download/v#{version}-Beta/NNDD_v#{version.dots_to_underscores}.dmg"
-  name 'NNDD+DMC'
-  homepage 'https://github.com/SSW-SCIENTIFIC/NNDD'
+  name "NNDD+DMC"
+  homepage "https://github.com/SSW-SCIENTIFIC/NNDD"
 
-  depends_on cask: 'adobe-air'
+  disable! date: "2026-09-13", because: "is no longer used"
 
-  app 'NNDD+DMC.app'
+  depends_on cask: "adobe-air"
+  depends_on :macos
+
+  app "NNDD+DMC.app"
 
   preflight do
-    system_command staged_path.join('Install NNDD+DMC.app/Contents/MacOS/Install NNDD+DMC'),
-                   args: ['-silent', '-eulaAccepted', '-location', staged_path]
+    system_command staged_path.join("Install NNDD+DMC.app/Contents/MacOS/Install NNDD+DMC"),
+                   args: ["-silent", "-eulaAccepted", "-location", staged_path]
   end
 end
