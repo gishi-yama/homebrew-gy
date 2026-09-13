3029ac1ff2677960381b6ff3388d08bb66ffcd7e
gishi-yama <yamakawa@photon.chitose.ac.jp>
2026-09-13 11:02:34 +0900

    コーポレートロゴフォントをfontスタンザに簡略化
    手動unar展開のpostflightを廃止し、標準fontスタンザで
    otf抽出・インストールする方式へ統一。

--- Casks/font-corporate-logo-b.rb
+++ Casks/font-corporate-logo-b.rb
@@ -6,36 +6,6 @@ cask 'font-corporate-logo-b' do
   name 'Corporate Logo Bold ver3'
   homepage 'https://logotype.jp/corporate-logo-font-dl.html'
 
-  # 自動展開を無効化
-  container type: :naked
-
-  postflight do
-    unar_path = "/opt/homebrew/bin/unar"
-    unless File.executable?(unar_path)
-      opoo "unar が見つかりません。unar をインストールするか、対応した unzip を利用してください。"
-      raise "unar が見つかりません"
-    else
-      tmp_dir = Dir.mktmpdir("corpgo_b_", "/private/tmp")
-      begin
-        zip_file = staged_path.join("Corporate-Logo-Bold-ver#{version}.zip")
-        system_command unar_path, args: [
-          "-quiet",
-          "-e", "cp932",
-          "-o", tmp_dir,
-          zip_file.to_s
-        ]
-        # 一時ディレクトリ以下を再帰的に検索して .otf ファイルを取得
-        font_file = Dir.glob(File.join(tmp_dir, "**", "*.otf")).first
-        raise "OTF ファイルが見つかりません" unless font_file
-  
-        target = File.expand_path("~/Library/Fonts/Corporate-Logo-Bold-ver3.otf")
-        # FileUtils.install は指定したパーミッションでコピーするため、
-        # chown の必要がなく、ユーザー所有となります。
-        FileUtils.install(font_file, target, mode: 0644)
-      ensure
-        FileUtils.remove_entry(tmp_dir) if tmp_dir && Dir.exist?(tmp_dir)
-      end
-    end
-  end
+  font "Corporate-Logo-Bold-ver#{version}/Corporate-Logo-Bold-ver#{version}.otf"
 
 end
